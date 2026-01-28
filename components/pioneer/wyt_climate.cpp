5df4f9a22c549206fbfb4633ac7da9004b5d7c97
Tyler Trafford <tyler@trafford.net>
2026-01-28 12:49:02 -0600

    feat: Add Pioneer WYT climate component with state query, control, and sensor integration.

--- components/pioneer/wyt_climate.cpp
+++ components/pioneer/wyt_climate.cpp
@@ -385,11 +385,11 @@ void WytClimate::switch_to_custom_fan_mode_(std::string custom_fan_mode) {
 
 void WytClimate::set_fan_mode_(climate::ClimateFanMode fan_mode) {
   this->fan_mode = fan_mode;
-  this->custom_fan_mode.reset();
+  climate::Climate::set_custom_fan_mode_(nullptr);
 }
 
-void WytClimate::set_custom_fan_mode_(const std::string &custom_fan_mode) {
-  this->custom_fan_mode = custom_fan_mode;
+void WytClimate::set_custom_fan_mode_(const char *custom_fan_mode) {
+  climate::Climate::set_custom_fan_mode_(custom_fan_mode);
   this->fan_mode.reset();
 }
 
